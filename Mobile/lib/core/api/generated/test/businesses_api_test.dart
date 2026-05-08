ca0bf808a8e8361bfd8d21ef345e5cce78b15396
ucEzette <u.ezette@alustudent.com>
2026-05-08 22:22:53 +0200

    Add mobile: businesses_api_test.dart & Add mobile: listings_api_test.dart

---
+++ Mobile/lib/core/api/generated/test/businesses_api_test.dart
@@ -0,0 +1,35 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.18
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:openapi/api.dart';
+import 'package:test/test.dart';
+
+
+/// tests for BusinessesApi
+void main() {
+  // final instance = BusinessesApi();
+
+  group('tests for BusinessesApi', () {
+    // Get all businesses
+    //
+    //Future businessesGet() async
+    test('test businessesGet', () async {
+      // TODO
+    });
+
+    // Create a new business
+    //
+    //Future businessesPost(BusinessesPostRequest businessesPostRequest) async
+    test('test businessesPost', () async {
+      // TODO
+    });
+
+  });
+}
---
+++ Mobile/lib/core/api/generated/test/listings_api_test.dart
@@ -0,0 +1,35 @@
+//
+// AUTO-GENERATED FILE, DO NOT MODIFY!
+//
+// @dart=2.18
+
+// ignore_for_file: unused_element, unused_import
+// ignore_for_file: always_put_required_named_parameters_first
+// ignore_for_file: constant_identifier_names
+// ignore_for_file: lines_longer_than_80_chars
+
+import 'package:openapi/api.dart';
+import 'package:test/test.dart';
+
+
+/// tests for ListingsApi
+void main() {
+  // final instance = ListingsApi();
+
+  group('tests for ListingsApi', () {
+    // Get all listings
+    //
+    //Future listingsGet({ String category }) async
+    test('test listingsGet', () async {
+      // TODO
+    });
+
+    // Create a new listing
+    //
+    //Future listingsPost(ListingsPostRequest listingsPostRequest) async
+    test('test listingsPost', () async {
+      // TODO
+    });
+
+  });
+}
