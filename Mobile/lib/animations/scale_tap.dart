b3e313ffd59b76624b554388c4ba6bdf391c2e5f
ucEzette <u.ezette@alustudent.com>
2026-05-08 21:21:51 +0200

    Add mobile: Fastfile & Add mobile: scale_tap.dart

---
+++ Mobile/lib/animations/scale_tap.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class ScaleTap extends StatefulWidget {
+  final Widget child;
+  final VoidCallback? onTap;
+  final double scale;
+
+  const ScaleTap({
+    super.key,
+    required this.child,
+    this.onTap,
+    this.scale = 0.95,
+  });
+
+  @override
+  State<ScaleTap> createState() => _ScaleTapState();
+}
+
+class _ScaleTapState extends State<ScaleTap> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 100),
+      vsync: this,
+    );
+    _animation = Tween<double>(begin: 1.0, end: widget.scale).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onTapDown(TapDownDetails details) {
+    if (widget.onTap != null) _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    if (widget.onTap != null) {
+      _controller.reverse();
+      widget.onTap?.call();
+    }
+  }
+
+  void _onTapCancel() {
+    if (widget.onTap != null) _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _onTapDown,
+      onTapUp: _onTapUp,
+      onTapCancel: _onTapCancel,
+      behavior: HitTestBehavior.opaque,
+      child: ScaleTransition(
+        scale: _animation,
+        child: widget.child,
+      ),
+    );
+  }
+}
